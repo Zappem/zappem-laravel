47eed7f75d1c63f57c1c26ce8cd893b32e1096b8
Dan <dan@danj.eu>
2016-08-17 21:43:41 +0100

    The class now grabs the file name from each item in the trace and a few lines of code from that file

--- src/Zappem.php
+++ src/Zappem.php
@@ -7,16 +7,92 @@ class Zappem{
 	public $User;
 	public $Data;
 
-	public function __construct($URL, $Project, $User=null){
+	private $autoload;
 
+	public function __construct($URL, $Project, $User=null){
 		$this->URL = $URL;
 		$this->Project = $Project;
 		$this->User = $User;
+		$this->autoload = require base_path().'/vendor/autoload.php';
 
 	}
 
+    private function getFileLines($start = 0, $length = null, $file){
+        if (null !== ($contents = file_get_contents($file))) {
+            $lines = explode("\n", $contents);
+
+            if ($length !== null) {
+                $start  = (int) $start;
+                $length = (int) $length;
+                if ($start < 0) {
+                    $start = 0;
+                }
+                $lines = array_slice($lines, $start, $length, true);
+            }
+            return $lines;
+        }
+    }
+
+    private function getFileFromClassName($class){
+    	return $this->autoload->findFile($class);
+    }
+
+    private function mainExceptionAsTrace($e){
+    	$Trace = [];
+    	$Trace['file'] = $e->getFile();
+    	$Trace['line'] = $e->getLine();
+    	$Trace['function'] = null;
+    	$Trace['class'] = get_class($e);
+    	$Trace['type'] = null;
+    	$Trace['args'] = null;
+    	$range = $this->getFileLines($Trace['line'] - 8, 10, $Trace['file']);
+		if($range){
+			$range = array_map(function($line){
+                return empty($line) ? ' ' : $line;
+            }, $range);
+
+            $start = key($range) + 1;
+            $code  = join("\n", $range);
+        
+            $Trace['file_contents'] = $code;
+		}
+		return $Trace;
+    }
+
+    private function processTrace($e){
+    	$Stack = [];
+
+    	$Stack[] = $this->mainExceptionAsTrace($e);
+
+    	foreach($e->getTrace() as $Trace){
+    		if(isset($Trace['class'])){
+	    		$Trace['file'] = $this->getFileFromClassName($Trace['class']);
+	    		if(isset($Trace['line'])){
+		    		$range = $this->getFileLines($Trace['line'] - 8, 10, $Trace['file']);
+
+		    		if($range){
+						$range = array_map(function($line){
+		                    return empty($line) ? ' ' : $line;
+		                }, $range);
+
+		                $start = key($range) + 1;
+		                $code  = join("\n", $range);
+		            
+		                $Trace['file_contents'] = $code;
+		    		}
+		    		$Trace['args'] = null;
+		    		$Stack[] = $Trace;
+		    	}
+		    }
+	    }
+
+	    return $Stack;
+    }
+
+
 	public function exception($e, $found_by=null){
 
+		$Trace = $this->processTrace($e);
 
 		$this->Data = [
 			"project_id" => $this->Project,
@@ -26,7 +102,7 @@ class Zappem{
 				"file"	  => $e->getFile(),
 				"line"	  => $e->getLine(),
 				"code"	  => $e->getCode(),
-				"trace" => $e->getTrace(),
+				"trace" => $Trace,
 				"get"	=> $_GET,
 				"post"	=> $_POST,
 				"server" => $_SERVER,
