2c4be9b1545a0702e6e50c9aa06764852b7f2c42
Dan Johnson <dan@danj.eu>
2016-11-21 23:44:04 +0000

    Now passes through a snippet of the file where the error occurred, and global variables. Also some changes to conform to the API

--- src/Zappem.php
+++ src/Zappem.php
@@ -15,7 +15,58 @@ class Zappem{
 		$this->User = $User;
 	}
 
+    public function getSource($file, $number, $radius = 2){
+        if (!is_file($file) or !is_readable($file)) {
+            return [];
+        }
+
+        $before = $after = $radius;
+        $start  = ($number - 1) - $before;
+
+        if ($start <= 0) {
+            $start  = 1;
+            $before = 1;
+        }
+
+        $duration = $before + 1 + $after;
+        $size     = $start + $duration;
+        $lines    = [];
+
+        $file_handle = fopen($file, 'r');
+
+        for ($l = 1; $l < $size; $l++) {
+            $line = fgets($file_handle);
+
+            if ($l < $start) {
+                continue;
+            }
+
+            $lines["$l"] = $this->trimLine($line);
+        }
+
+        return $lines;
+    }
+
+    private function trimLine($line){
+        $trimmed = trim($line, "\n\r\0\x0B");
+
+        return preg_replace(
+            [
+                '/\s*$/D',
+                '/\t/'
+            ],
+            [
+                '',
+                '    ',
+            ],
+            $trimmed
+        );
+    }
+
 	public function exception($e, $found_by=null){
+
+		$Trace = $e->getTrace();
+		$Source = $this->getSource($Trace[0]['file'], $Trace[0]['line']);
 
 		$this->Data = [
 			"project" => $this->Project,
@@ -23,11 +74,15 @@ class Zappem{
 			"url" => $_SERVER['REQUEST_URI'],
 			"ip" => $_SERVER['REMOTE_ADDR'],
 			"useragent" => $_SERVER['HTTP_USER_AGENT'],
+			"env" => $_ENV,
+			"cookies" => $_COOKIE,
+			"data" => ($_SERVER['REQUEST_METHOD'] == "post" ? $_POST : $_GET),
 			"message" => $e->getMessage() ? $e->getMessage() : get_class($e),
 			"class" => get_class($e),
 			"file" => $e->getFile(),
 			"line" => $e->getLine(),
-			"trace" => $e->getTrace()
+			"trace" => $e->getTrace(),
+			"source" => $Source
 		];
 
 		return $this;
